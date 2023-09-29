69a6d57212f30ea6c0ee35f62ebb39ac8e87a5f8
saveligulas <saveli.gulas@gmail.com>
2023-09-29 22:03:23 +0200

    updates

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ myList = MyCollection()
 for i in range(10):
     myList.add(i)
 myList.add_end(999)
-
 for i in range(5):
     myList.remove_index(0)
+myList.remove_object(999)
 print(myList.__str__())
 
