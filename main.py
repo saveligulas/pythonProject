4049de767628003fe9d6adcce72e635be937111b
saveligulas <saveli.gulas@gmail.com>
2023-09-29 21:55:43 +0200

    updates

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from MyCollection import MyCollection
 myList = MyCollection()
 for i in range(10):
     myList.add(i)
-myList.remove_index(1)
+myList.add_end(999)
+
+for i in range(5):
+    myList.remove_index(0)
 print(myList.__str__())
 
